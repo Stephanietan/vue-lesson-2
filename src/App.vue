8ee1b4d640ceb679cd84aa374811af7830b82bad
cavacado <ngzhaoloong@gmail.com>
2017-02-26 22:24:14 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     {{ msg }}
+    <child-comp></child-comp>
   </div>
 </template>
 
 <script>
+import Child from './components/Child.vue';
+
 export default {
+  components: { 'child-comp' : Child },
   data: function() {
     return {
       msg : 'Hello world!'
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,9 +1,15 @@
 <template>
-
+  <p>{{ anotherMsg }}</p>
 </template>
 
 <script>
-
+  export default {
+    data: function() {
+      return {
+        anotherMsg: 'Hello Universe!'
+      }
+    }
+  }
 </script>
 
 <style>
