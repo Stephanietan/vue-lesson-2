6aacf824dc071ce56524e4800a10cd781e5fdd7b
cavacado <ngzhaoloong@gmail.com>
2017-02-27 00:36:09 +0800

    setup

--- src/Parent.vue
+++ src/Parent.vue
@@ -2,14 +2,16 @@
   <div>
     <p>I am the parent component</p>
     <child-comp :msg-from-parent='text' @emitMutateEvent='text = $event' :mutateFn='mutateMe'></child-comp>
+    <sibling-comp></sibling-comp>
   </div>
 </template>
 
 <script>
 import Child from './components/Child.vue';
+import Sibling from './components/Sibling.vue';
 
 export default {
-  components: { 'child-comp' : Child },
+  components: { 'child-comp' : Child, 'sibling-comp' : Sibling },
   data: function() {
     return {
       msg : 'Hello world!',
---
+++ src/components/Sibling.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>  
+    <div>I am the sibling component</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style></style>
